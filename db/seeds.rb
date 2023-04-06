c71a270409bfdb786dc4479655f56809a51c1701
wandergithub <wandergonzalez0@hotmail.com>
2023-04-06 00:39:43 -0400

    Create seed file to populate database with sample info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create some strengths
+s1 = Strength.create(name: "Communication", proficiency: "Expert", recomendations: 10)
+s2 = Strength.create(name: "Leadership", proficiency: "Intermediate", recomendations: 5)
+s3 = Strength.create(name: "Problem-solving", proficiency: "Advanced", recomendations: 7)
+
+# Create some users
+u1 = User.create(name: "John Doe", picture: "https://example.com/picture1.jpg")
+u2 = User.create(name: "Jane Smith", picture: "https://example.com/picture2.jpg")
+
+# Associate users with their strengths
+u1.strengths << s1
+u1.strengths << s2
+u2.strengths << s2
+u2.strengths << s3
+
+# Create some experiences
+e1 = Experience.create(name: "Software Engineer", organization: "Acme Inc.", fromMonth: "January", fromYear: 2019, user: u1)
+e2 = Experience.create(name: "Marketing Manager", organization: "Globex Corp.", fromMonth: "June", fromYear: 2015, user: u2)
