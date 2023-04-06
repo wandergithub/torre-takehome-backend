281aeaa9dcd6636ba73fb0f28230d87f37c29e14
wandergithub <wandergonzalez0@hotmail.com>
2023-04-06 10:28:05 -0400

    Add experiences and strengths on show user by name request response

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
     def show_by_name
       name = params[:name]
       users = User.where("name ~* ?", name) # the `~*` operator performs a case-insensitive regex match
-      render json: users.first
+      render json: users.first.to_json(include: [:experiences, :strengths])
     end
   end
   
