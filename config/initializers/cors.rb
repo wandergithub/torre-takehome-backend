309d82350f8aa360c6976a777e98cb1e42f82b2f
wandergithub <wandergonzalez0@hotmail.com>
2023-04-06 16:13:05 -0400

    Change cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://torre-wander.netlify.app/'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
