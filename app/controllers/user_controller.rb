c774eef095d3983cd0bfd8535c2efece92db8d16
wandergithub <wandergonzalez0@hotmail.com>
2023-04-06 10:34:30 -0400

    Add route and method to allow all users get request

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,10 @@
 class UserController < ApplicationController
+    def index
+        users = User.all
+
+        render json: users
+    end
+
     def show_by_name
       name = params[:name]
       users = User.where("name ~* ?", name) # the `~*` operator performs a case-insensitive regex match
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'users/:name', to: 'user#show_by_name'
+  get 'users', to: 'user#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'users/:name', to: 'user#show_by_name'
 end
